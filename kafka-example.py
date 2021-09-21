dfd898279f21dadcb342d43de77f392dfd3547e2
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2021-09-21 16:57:10 -0300

    Removing unused code

--- kafka-example.py
+++ kafka-example.py
@@ -4,25 +4,6 @@ from kafka import KafkaAdminClient, KafkaConsumer, KafkaProducer
 from kafka.admin import NewTopic
 
 
-class Producer(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.stop_event = threading.Event()
-
-    def stop(self):
-        self.stop_event.set()
-
-    def run(self):
-        producer = KafkaProducer(bootstrap_servers='localhost:9092')
-
-        while not self.stop_event.is_set():
-            producer.send('my-topic', b"test")
-            producer.send('my-topic', b"\xc2Hola, mundo!")
-            time.sleep(1)
-
-        producer.close()
-
-
 class Consumer(threading.Thread):
     def __init__(self, topic_name):
         threading.Thread.__init__(self)
@@ -54,19 +35,12 @@ def main():
     # Create 'my-topic' Kafka topic
     try:
         admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
-
-        # topic = NewTopic(name='dbserver1.inventory.customers',
-        #                  num_partitions=1,
-        #                  replication_factor=1)
-        # admin.create_topics([topic])
     except Exception:
         pass
 
     tasks = [
         Consumer('dbserver1.inventory.customers')
     ]
-
-    # Start threads of a publisher/producer and a subscriber/consumer to 'my-topic' Kafka topic
     for t in tasks:
         t.start()
 
